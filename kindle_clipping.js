a12200c7b1467bac49bcb9249e1ffc1c0f838750
Hui <am@itti.cc>
2014-10-14 17:28:55 +0800

    Update kindle_clipping.js

--- kindle_clipping.js
+++ kindle_clipping.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 
 var helper = {
-    split_text: function(text) {
-        var re = /#(\d+\-?\d+)\s*的(.*)\s+\|\s+添加于\s+(\d+)年(\d+)月(\d+)日.*(上|下)午(\d+):(\d+):(\d+)$/,
+    split_created: function(text) {
+        var re = /#(\d+\-?\d+)\s*的(.*)\s+\|\s+添加于\s+(\d{4})年(\d{2})月(\d{2})日.*(上|下)午(\d{2}):(\d{2}):(\d{2})$/,
             match = re.exec(text);
 
         var m7 = +match[7];
@@ -16,13 +16,23 @@ var helper = {
             type: match[2],
             date: new Date(match[3] + ' ' + match[4] + ' ' + match[5] + ' ' + hours + ':' + minutes + ':' + seconds)
         };
+    },
+    split_book: function(text) {
+        var re = /(.+)\((.+)\)/,
+            match = re.exec(text);
+
+        return {
+            book: match[1].trim(),
+            author: match[2].trim()
+        };
     }
 };
 
 function Block(texts) {
     this.type = ''; // '标注'，'书签'
     this.position = '';
-    this.title = '';
+    this.book = '';
+    this.author = '';
     this.date = null;
     this.content = '';
     this.texts = texts;
@@ -33,17 +43,23 @@ function Block(texts) {
 Block.prototype.init = function() {
     var para_arr = this.texts.split('\r\n');
 
-    this.title = para_arr[0];
-    this.content = para_arr[3];
-
     var data = null;
 
+    data = helper.split_bool(para_arr[0]);
+    this.author = data.author;
+    this.book = data.book;
+
+    this.content = para_arr[3];
+
+
     if(para_arr[1]) {
-        data = helper.split_text(para_arr[1]);
+        data = helper.split_created(para_arr[1]);
         this.position = data.position;
         this.type = data.type;
         this.date = data.date;
     }
+
+    data = null;
 };
 
 function parse_blocks(paragraphs) {
@@ -74,3 +90,4 @@ function kindle_clippings(path) {
 }
 
 kindle_clippings('./My Clippings.txt');
+
